c02a2d5f3da97ce081bf7dd29cf55ef79e6c08ee
Dylan Dodds <Dylan.Dodds@rainclouds.io>
2025-11-21 12:01:20 -0500

    Updated structure of garden

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://oddlydoddly.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/oddlys-garden/',
+  baseUrl: process.env.NODE_ENV === 'development' ? '/' : '/oddlys-garden/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
